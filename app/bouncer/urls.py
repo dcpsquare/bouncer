c5c0c547a342b39de672a151b0efad173da36f0a
dcpsquare <deepak.pandey@thesqua.re>
2021-09-23 11:50:28 +0530

    deploy last

--- app/bouncer/urls.py
+++ app/bouncer/urls.py
@@ -12,6 +12,7 @@ from bouncer import views
 app_name = 'bouncer'
 
 urlpatterns =[
+    path('<str:slug>/', views.handle_redirect, name='redirect'),
     path('', views.landing, name='landing'),
 
 ]
